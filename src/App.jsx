2a92d535a0c3c7ee64110b310bc83dc97aa39f38
Quiroz <cristianquiroz2311@gmail.com>
2024-06-20 17:35:14 -0400

    Curso de Carrito desde 58-66

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
+import { useState, useEffect } from "react";
+import { db } from "./data/db";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(db);
+  }, []);
+
   return (
     <>
       <Header />
@@ -11,7 +18,12 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
+          {data.map((guitar) => (
+            <Guitar
+            key={guitar.id} 
+            guitar ={guitar} 
+            />
+          ))}
         </div>
       </main>
 
